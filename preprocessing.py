7e23a6c436b35f719cf10b5188aa55dea30b8f13
tmralmeida <tmr.almeida96@gmail.com>
2022-02-05 13:52:05 +0100

    more automatic behavior

---
+++ preprocessing.py
@@ -0,0 +1,220 @@
+import pandas as pd
+import re
+import numpy as np
+import _pickle as cPickle
+from typing import List
+import os
+
+
+class Preprocessor():
+    def __init__(self, curr_exp, curr_run, cfg) -> None:
+        data_cfg = cfg["data"]
+        params_cfg = cfg["parameters"]
+        self.exp = curr_exp
+        self.run = curr_run
+        self.path = os.path.join(data_cfg["path"], f"Exp_{self.exp}_run_{self.run}_6D.tsv")
+        self.save_dir = data_cfg["save_dir"]
+        
+        self.res_rule = params_cfg["resample_rule"]
+        self.avg_window = params_cfg["average_window"]
+        obs_len = params_cfg["obs_len"]
+        pred_len = params_cfg["pred_len"]
+        self.full_len = obs_len + pred_len
+        
+        
+        
+    def __loading(self) -> pd.DataFrame:
+        # initial df
+        df = pd.read_csv(self.path, sep = "\t", header = 10).dropna(axis = 1)
+        
+        # changing column names
+        col_list = df.columns.tolist()[1:]
+        new_columns = [df.columns.tolist()[0]]
+        prev_col_name = col_list[0]
+        for column in col_list:
+            if re.match("Y.*", column) and re.match("Helmet.*",prev_col_name):
+                new_columns.append(prev_col_name + " Y")
+            else:
+                new_columns.append(column)
+            prev_col_name = column.split(" ")[0]
+        df.columns = new_columns
+        # Checking if all data:
+        # * 13 rigid bodies
+        # * 16 coordinates
+        # * 2 columns for time and frame
+        assert df.shape[1] == 16*13 + 2 
+        return df
+    
+    
+    def __get_rolesmap(self):
+        if self.exp == 1:
+            if self.run== 1:
+                n_groups = 2
+                visitors_group1 = [6,7,5] 
+                visitors_group2 = [8,2,4]
+            elif self.run== 2:
+                n_groups = 2
+                visitors_group1 = [2,6,7,5] 
+                visitors_group2 = [8, 4]
+            elif self.run== 3:
+                n_groups = 3
+                visitors_group1 = [6,7,8]
+                visitors_group2 = [4,5]
+                visitors_group3 = [2]
+            elif self.run== 4:
+                n_groups = 2
+                visitors_group1 = [2, 4, 5, 7, 8]
+                visitors_group2 = [6]
+            
+            workers_utility = [3]
+            workers_lab = [9]
+
+        elif self.exp == 2:
+            if self.run== 1:
+                n_groups = 2
+                visitors_group1 = [4,5,6] 
+                visitors_group2 = [3,7,9]
+            elif self.run== 2:
+                n_groups = 2
+                visitors_group1 = [3,5,6,9] 
+                visitors_group2 = [7, 4]
+            elif self.run== 3:
+                n_groups = 3
+                visitors_group1 = [5,7,9]
+                visitors_group2 = [4,6]
+                visitors_group3 = [3]
+            elif self.run== 4:
+                n_groups = 2
+                visitors_group1 = [3,5,6,7,9]
+                visitors_group2 = [4]
+            elif self.run== 5:
+                n_groups = 3
+                visitors_group1 = [3, 6]
+                visitors_group2 = [4, 9]
+                visitors_group3 = [5, 7]
+            
+            workers_utility = [2]
+            workers_lab = [8]
+            
+        elif self.exp == 3:
+            if self.run== 1:
+                n_groups = 2
+                visitors_group1 = [2,3,8] 
+                visitors_group2 = [6,7,9]
+            elif self.run== 2:
+                n_groups = 2
+                visitors_group1 = [2,8,9] 
+                visitors_group2 = [3,6,7]
+            elif self.run== 3:
+                n_groups = 3
+                visitors_group1 = [2,3,7]
+                visitors_group2 = [8,9]
+                visitors_group3 = [6]
+            elif self.run== 4:
+                n_groups = 2
+                visitors_group1 = [2,3,6,7,9]
+                visitors_group2 = [8]
+            
+            workers_utility = [5]
+            workers_lab = [4]
+        
+        inspector = [10]
+
+        roles = {
+            "VISITOR" : visitors_group1 + visitors_group2 if n_groups == 2 else visitors_group1 + visitors_group2 + visitors_group3,
+            "WORKER" : workers_lab + workers_utility,
+            "inspector" : inspector,
+        }
+        return roles
+    
+    
+    def __splitting_helms(self, df : pd.DataFrame) -> pd.DataFrame:
+        roles = self.__get_rolesmap()
+        col_list = df.columns.tolist()
+        dfs_helmets = []
+        prev_col_name = col_list[0]
+        time_col = df["Time"]
+        for column in col_list:
+            if re.match(".*Y", column) and re.match(".*X",prev_col_name):
+                df_targ = df[[prev_col_name, column]]
+                df_targ.columns = ["X", "Y"]
+                helmet_n = int(re.findall("\d+", column)[0])
+                if helmet_n == 1:
+                    continue
+                    
+                # interpolation
+                df_targ = df_targ.replace(0, np.nan)
+                df_targ = df_targ.interpolate()
+                
+                # average filter
+                df_average = df_targ.copy()
+                df_average.loc[:, "Time"] = time_col
+                df_average["date"] = pd.TimedeltaIndex(time_col, unit = "s")
+                df_average = df_average.rolling("500ms", on = "date").mean()
+                df_average.loc[:, "Frame"] = np.arange(1, df_average.shape[0] + 1)
+                for idx_d, r in enumerate(roles.values()):
+                    if helmet_n in r:
+                        idx_dict = idx_d
+                df_final =  df_average.assign(Description = list(roles.keys())[idx_dict])
+                df_final =  df_final.assign(Person_ID = helmet_n)
+                
+                df_final = df_final[["Frame", "Time", "Person_ID", "X", "Y", "Description"]]
+                dfs_helmets.append(df_final)
+            prev_col_name = column
+        # concatenating and ordering
+        df_concatenated = pd.concat(dfs_helmets)
+        df_sorted = df_concatenated.sort_values(by=["Frame", "Person_ID"]).dropna().reset_index(drop=True)
+        return df_sorted
+    
+    
+    def __extract_subtrajs(self, df : pd.DataFrame) -> List[pd.DataFrame]:
+        n_helmets = df.Person_ID.unique()
+        sub_trajs = []
+        for h in n_helmets:
+            hel_data = df[df.Person_ID == h]
+            n_steps = hel_data.shape[0] 
+            if n_steps >= (self.full_len):
+                n_chunks = int(n_steps / (self.full_len))
+                chunks = np.array_split(hel_data, n_chunks)
+                targ_chunks = list(filter(lambda x : x.shape[0] == (self.full_len), chunks))   
+                sub_trajs.extend(targ_chunks)
+        return sub_trajs
+    
+    
+    def preprocessing(self) -> List[pd.DataFrame]:
+        df = self.__loading()
+        # merge dfs -> frame time + helmets
+        merged_df = pd.merge(df[["Frame", "Time"]], df.filter(regex=("Helmet.*")), left_index=True, right_index=True)
+
+        # create time col for resampling
+        merged_df["date"] = pd.TimedeltaIndex(merged_df.Time, unit = "s")
+        
+        # resampling
+        no_frame = merged_df.drop("Frame", axis = 1)
+        no_frame_resampled = no_frame.resample(rule=self.res_rule, on = "date").mean()
+        
+        # splitting helmets and ordering
+        ordered_df = self.__splitting_helms(no_frame_resampled)
+        print(f"Data filtered with {ordered_df.shape[0]} data points!")
+        
+        # split into sub trajs
+        sub_trajs = self.__extract_subtrajs(ordered_df)
+        return sub_trajs
+    
+    
+    def save_files(self, sub_trajs : List[pd.DataFrame]) -> None:
+        print(f"Exporting {len(sub_trajs)} trajectories!")
+        if not os.path.exists(self.save_dir):
+            print("Save dir doesn't exist. Creating save dir...")
+            os.makedirs(self.save_dir)
+            
+        fn = f"EXP{self.exp}_RUN_{self.run}_sub_trajs.pkl"
+        with open(os.path.join(self.save_dir, fn), "wb") as f:
+            cPickle.dump(sub_trajs, f)
+
+        print(f"File {fn} saved in {self.save_dir}.")
+        
+        
+        
+        
+        
--- run.py
+++ run.py
@@ -1,12 +1,7 @@
-from operator import sub
-import pandas as pd
 import os
-import re
-import numpy as np
 import yaml
 from argparse import ArgumentParser
-import _pickle as cPickle
-from .roles import get_roles_mapping
+from .preprocessing import Preprocessor
 
 parser = ArgumentParser(description = "Preprocess THOR dataset")
 
@@ -22,119 +17,21 @@ args = parser.parse_args()
 
 with open(args.cfg, "r") as f:
     cfg = yaml.safe_load(f)
-data_info = cfg["data"]
-params = cfg["parameters"]
 
-# data
-exp = data_info["experiment"]
-run = data_info["run"]
-path = data_info["path"]
-save_dir = data_info["save_dir"]
+exps = cfg["data"]["experiments"]
+runs = cfg["data"]["runs"]
+exp_run = list(zip(exps, runs))
 
-# parameters
-resample_rule = params["resample_rule"]
-average_window = params["average_window"]
-obs_len = params["obs_len"]
-pred_len = params["pred_len"]
-full_traj_len = obs_len + pred_len
+total_nst = []
+for er in exp_run:
+    exp = er[0]
+    run = range(1, er[1] + 1)
+    for r in run:
+        pp = Preprocessor(exp, r, cfg)
+        st = pp.preprocessing()
+        pp.save_files(st)
+        total_nst.append(len(st))
+ 
 
-# path 
-exp_path =  os.path.join(path, f"Exp_{exp}_run_{run}_6D.tsv")
+print(f"Preprocessed a total amount of {sum(total_nst)} trajectories.")
 
-
-# initial df
-df = pd.read_csv(exp_path, sep = "\t", header = 10).dropna(axis = 1)
-
-# changing column names
-col_list = df.columns.tolist()
-new_columns = []
-prev_column = col_list[0]
-for i, column in enumerate(col_list):
-    if re.match("Y.*", column) and re.match("Helmet.*",prev_col_name):
-        new_columns.append(prev_col_name + " Y")
-    else:
-        new_columns.append(column)
-    prev_col_name = column.split(" ")[0]
-df.columns = new_columns
-
-
-# Checking if all data is here:
-# * 13 rigid bodies
-# * 16 coordinates
-# * 2 columns for time and frame
-assert df.shape[1] == 16*13 + 2 
-
-# merge dfs -> frame time + helmets
-merged_df = pd.merge(df[["Frame", "Time"]], df.filter(regex=("Helmet.*")), left_index=True, right_index=True)
-
-# create time col for resampling
-merged_df["date"] = pd.TimedeltaIndex(merged_df.Time, unit = "s")
-
-# resampling
-no_frame = merged_df.drop("Frame", axis = 1)
-no_frame_resampled = no_frame.resample(rule=resample_rule, on = "date").mean()
-
-# splitting each helmet's traj, interpolating, and smooting
-roles = get_roles_mapping(experiment = exp, run = run)
-col_list = no_frame_resampled.columns.tolist()
-dfs_helmets = []
-prev_col_name = col_list[0]
-time_col = no_frame_resampled["Time"]
-for i, column in enumerate(col_list):
-    if re.match(".*Y", column) and re.match(".*X",prev_col_name):
-        df_targ = no_frame_resampled[[prev_col_name, column]]
-        df_targ.columns = ["X", "Y"]
-        helmet_n = int(re.findall("\d+", column)[0])
-        if helmet_n == 1:
-            continue
-            
-        # interpolation
-        df_targ = df_targ.replace(0, np.nan)
-        df_targ = df_targ.interpolate()
-        
-        # average filter
-        df_average = df_targ.copy()
-        df_average.loc[:, "Time"] = time_col
-        df_average["date"] = pd.TimedeltaIndex(time_col, unit = "s")
-        df_average = df_average.rolling("500ms", on = "date").mean()
-        df_average.loc[:, "Frame"] = np.arange(1, df_average.shape[0] + 1)
-        for idx_d, r in enumerate(roles.values()):
-            if helmet_n in r:
-                idx_dict = idx_d
-        df_final =  df_average.assign(Description = list(roles.keys())[idx_dict])
-        df_final =  df_final.assign(Person_ID = helmet_n)
-        
-        df_final = df_final[["Frame", "Time", "Person_ID", "X", "Y", "Description"]]
-        dfs_helmets.append(df_final)
-    prev_col_name = column
-
-
-# concatenating and ordering
-df_concatenated = pd.concat(dfs_helmets)
-df_sorted = df_concatenated.sort_values(by=["Frame", "Person_ID"]).dropna().reset_index(drop=True)
-
-print(f"Data filtered with {df_sorted.shape[0]} data points!")
-
-# split into sub trajs
-n_helmets = df_sorted.Person_ID.unique()
-sub_trajs = []
-for h in n_helmets:
-    hel_data = df_sorted[df_sorted.Person_ID == h]
-    n_steps = hel_data.shape[0] 
-    if n_steps >= (full_traj_len):
-        n_chunks = int(n_steps / (full_traj_len))
-        chunks = np.array_split(hel_data, n_chunks)
-        targ_chunks = list(filter(lambda x : x.shape[0] == (full_traj_len), chunks))   
-        sub_trajs.extend(targ_chunks) 
-        
-print(f"Exporting {len(sub_trajs)} trajectories!")
-
-if not os.path.exists(save_dir):
-    print("Save dir doesn't exist. Creating save dir...")
-    os.makedirs(save_dir)
-    
-fn = f"EXP{exp}_RUN_{run}_sub_trajs.pkl"
-with open(os.path.join(save_dir, fn), "wb") as f:
-    cPickle.dump(sub_trajs, f)
-
-print(f"File {fn} saved in {save_dir}.")
